14d92c97bacdbbea2e7c76e0c5a29e38844591cb
Gabriel Oliveira <158515920+GrayHawkins@users.noreply.github.com>
2024-09-11 14:22:35 -0300

    Add files via upload

---
+++ python aula 1.py
@@ -0,0 +1,14 @@
+print("Gabriel")
+print('Gabriel')
+print('Gabriel Augusto Costa "Grayhawk" de Oliveira')
+print("Gabriel Augusto Costa \"Grayhawk\" de Oliveira")
+
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
