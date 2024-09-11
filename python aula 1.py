56f753ff2c13f6c1aec7bfe0f143e9bd1d8c72b4
Gabriel Oliveira <158515920+GrayHawkins@users.noreply.github.com>
2024-09-11 14:29:11 -0300

    testes

--- python aula 1.py
+++ python aula 1.py
@@ -1,4 +1,6 @@
-print("Gabriel")
+# Aqui eu posso adicionar comentários
+#continuar na linha de baixo
+print("Gabriel") # comentário a linha do código
 print('Gabriel')
 print('Gabriel Augusto Costa "Grayhawk" de Oliveira')
 print("Gabriel Augusto Costa \"Grayhawk\" de Oliveira")
@@ -10,5 +12,8 @@ teste = 17
 print(teste)
 print(type(teste))
 teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
 print(teste)
 print(type(teste))
